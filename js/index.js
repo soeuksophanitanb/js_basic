871aa13d97e3bb4060b8ac71bad3c72579319c23
soeuksophanitanb <sophanitsoeuk@gmail.com>
2023-06-29 18:35:23 +0700

    adding more

--- js/index.js
+++ js/index.js
@@ -97,3 +97,48 @@ while (num < 10) {
   console.log("Num ", num + 1);
   num++;
 }
+
+// ternary operator
+
+let score = 10;
+let grade = score < 100 ? "F" : "Pass";
+console.log(grade);
+
+let a = false,
+  b = true;
+
+let isTrue = a && b;
+let isFail = !isTrue;
+console.log(isFail);
+
+let time = 12;
+if (time >= 6 && time < 12) console.log("Good Morning");
+else if (time >= 12 && time < 17) console.log("Good Afternoon");
+else if (time >= 5 && time < 21) console.log("Good Evening");
+else console.log("Good Night");
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i, "Hello AnB School");
+}
+
+const staff = {
+  name: "John",
+  age: 22,
+};
+
+for (let key in staff) console.log(key, staff[key]);
+
+const newArr = [1, 2, 3, 4, 5];
+for (let element of newArr) console.log(element);
+
+function minMax(a, b) {
+  return a > b ? a : b;
+}
+
+console.log("Here is ", minMax(1, 2));
+
+function isLandscape(width, height) {
+  return width < height ? "is Portrait" : "is Landscape";
+}
+
+console.log(isLandscape(200, 300));
